49b2a623a81307fd2aa9dcb2d5fbdd52dd074d2c
Samal Roman <roman.samal.bs@gmail.com>
2017-11-22 22:44:31 +0300

    Add displaying WPD info.

--- IAPD-USB/MainWindow.cpp
+++ IAPD-USB/MainWindow.cpp
@@ -13,9 +13,9 @@ void MainWindow::InitializeComponent()
 	list->View = View::Details;
 	list->Columns->Add(LISTVIEW_COL1, LISTVIEW_COL1_WIDTH, HorizontalAlignment::Center);
 	list->Columns->Add(LISTVIEW_COL2, LISTVIEW_COL2_WIDTH, HorizontalAlignment::Center);
-	list->Columns->Add(LISTVIEW_COL3, LISTVIEW_COL3_WIDTH, HorizontalAlignment::Center);
-	list->Columns->Add(LISTVIEW_COL4, LISTVIEW_COL4_WIDTH, HorizontalAlignment::Center);
-	list->Columns->Add(LISTVIEW_COL5, LISTVIEW_COL5_WIDTH, HorizontalAlignment::Center);
+	list->Columns->Add(LISTVIEW_COL3, LISTVIEW_COL3_WIDTH, HorizontalAlignment::Left);
+	list->Columns->Add(LISTVIEW_COL4, LISTVIEW_COL4_WIDTH, HorizontalAlignment::Left);
+	list->Columns->Add(LISTVIEW_COL5, LISTVIEW_COL5_WIDTH, HorizontalAlignment::Left);
 	list->Location = Point(LISTVIEW_X, LISTVIEW_Y);
 	this->components = gcnew System::ComponentModel::Container();
 	this->Size = System::Drawing::Size(WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -60,14 +60,41 @@ void MainWindow::RefreshListView()
 		item->SubItems->Add(totalManaged);
 		item->SubItems->Add(freeManaged);
 		item->SubItems->Add(busyManaged);
-		item->SubItems->Add("1231414");
 		list->Items->Add(item);
 	}
 	WindowsPortableDevice wpd;
 	DWORD count = wpd.getDeviceCount();
+	PORTABLE_DEVICES *portable = wpd.getDevices();
 	for (int i = 0; i < count; i++)
 	{
-	}
+		if (portable[i].deviceName == L"" && portable[i].count == 0)
+			continue;
+		wstring deviceName = portable[i].deviceName;
+		String^ name = gcnew String(portable[i].deviceName.c_str());
+		ListViewItem ^item = gcnew ListViewItem(name);
+		list->Items->Add(item);
+		for (int j = 0; j < portable[i].count; j++)
+		{
+			double totalDb = (double)portable[i].total.at(j) / 1000000;
+			double freeDb = (double)portable[i].free.at(j) / 1000000;
+			double busyDb = (double)totalDb - freeDb;
+			wstring tempName = deviceName + L" : " + portable[i].storName.at(j);
+			String^ storname = gcnew String(tempName.c_str());
+			string total = std::to_string(totalDb);
+			String^ totalManaged = gcnew String(total.c_str());
+			string free = std::to_string(freeDb);
+			String^ freeManaged = gcnew String(free.c_str());
+			string busy = std::to_string(busyDb);
+			String^ busyManaged = gcnew String(busy.c_str());
+			ListViewItem ^storeItem = gcnew ListViewItem(storname);
+			storeItem->SubItems->Add("");
+			storeItem->SubItems->Add(totalManaged);
+			storeItem->SubItems->Add(freeManaged);
+			storeItem->SubItems->Add(busyManaged);
+			list->Items->Add(storeItem);
+		}
+		}
+		
 	return;
 }
 
